a4471be7240976ae0da55d01e016b3ad9ad85457
Misbah Ansori <misbah.ansori24@gmail.com>
2022-03-25 14:39:52 +0700

    Wip refactor

--- src/lib/components/Board.svelte
+++ src/lib/components/Board.svelte
@@ -1,221 +1,84 @@
-<line
-	x1="100"
-	y1="300"
-	x2="900"
-	y2="300"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<line
-	x1="100"
-	y1="500"
-	x2="900"
-	y2="500"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<line
-	x1="100"
-	y1="700"
-	x2="900"
-	y2="700"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<line
-	x1="100"
-	y1="900"
-	x2="900"
-	y2="900"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<line
-	x1="100"
-	y1="1100"
-	x2="900"
-	y2="1100"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<line
-	x1="100"
-	y1="1100"
-	x2="100"
-	y2="300"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<line
-	x1="300"
-	y1="1100"
-	x2="300"
-	y2="300"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<line
-	x1="500"
-	y1="1300"
-	x2="500"
-	y2="100"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<line
-	x1="700"
-	y1="1100"
-	x2="700"
-	y2="300"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<line
-	x1="900"
-	y1="1100"
-	x2="900"
-	y2="300"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<line
-	x1="100"
-	y1="700"
-	x2="700"
-	y2="1300"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<line
-	x1="900"
-	y1="700"
-	x2="300"
-	y2="1300"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<line
-	x1="100"
-	y1="700"
-	x2="700"
-	y2="100"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<line
-	x1="900"
-	y1="700"
-	x2="300"
-	y2="100"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<line
-	x1="300"
-	y1="100"
-	x2="700"
-	y2="100"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<line
-	x1="300"
-	y1="1300"
-	x2="700"
-	y2="1300"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<line
-	x1="400"
-	y1="200"
-	x2="600"
-	y2="200"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<line
-	x1="400"
-	y1="1200"
-	x2="600"
-	y2="1200"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<line
-	x1="100"
-	y1="300"
-	x2="900"
-	y2="1100"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<line
-	x1="900"
-	y1="300"
-	x2="100"
-	y2="1100"
-	stroke="#525252"
-	stroke-linecap="round"
-	stroke-dasharray="6 6"
-/>
-<text x="100" y="300" class="text-gray-500 text-sm fill-current">[100,300]</text>
-<text x="100" y="500" class="text-gray-500 text-sm fill-current">[100,500]</text>
-<text x="300" y="500" class="text-gray-500 text-sm fill-current">[300,500]</text>
-<text x="500" y="500" class="text-gray-500 text-sm fill-current">[500,500]</text>
-<text x="700" y="500" class="text-gray-500 text-sm fill-current">[700,500]</text>
-<text x="100" y="700" class="text-gray-500 text-sm fill-current">[100,700]</text>
-<text x="300" y="700" class="text-gray-500 text-sm fill-current">[300,700]</text>
-<text x="500" y="700" class="text-gray-500 text-sm fill-current">[500,700]</text>
-<text x="700" y="700" class="text-gray-500 text-sm fill-current">[700,700]</text>
-<text x="100" y="900" class="text-gray-500 text-sm fill-current">[100,900]</text>
-<text x="300" y="900" class="text-gray-500 text-sm fill-current">[300,900]</text>
-<text x="500" y="900" class="text-gray-500 text-sm fill-current">[500,900]</text>
-<text x="700" y="900" class="text-gray-500 text-sm fill-current">[700,900]</text>
-<text x="100" y="1100" class="text-gray-500 text-sm fill-current">[100,1100]</text>
-<text x="100" y="1100" class="text-gray-500 text-sm fill-current">[100,1100]</text>
-<text x="300" y="1100" class="text-gray-500 text-sm fill-current">[300,1100]</text>
-<text x="500" y="1100" class="text-gray-500 text-sm fill-current">[500,1100]</text>
-<text x="500" y="1300" class="text-gray-500 text-sm fill-current">[500,1300]</text>
-<text x="700" y="1100" class="text-gray-500 text-sm fill-current">[700,1100]</text>
-<text x="300" y="100" class="text-gray-500 text-sm fill-current">[300,100]</text>
-<text x="300" y="1300" class="text-gray-500 text-sm fill-current">[300,1300]</text>
-<text x="400" y="200" class="text-gray-500 text-sm fill-current">[400,200]</text>
-<text x="400" y="1200" class="text-gray-500 text-sm fill-current">[400,1200]</text>
-<text x="900" y="500" class="text-gray-500 text-sm fill-current">[900,500]</text>
-<text x="900" y="700" class="text-gray-500 text-sm fill-current">[900,700]</text>
-<text x="900" y="900" class="text-gray-500 text-sm fill-current">[900,900]</text>
-<text x="300" y="300" class="text-gray-500 text-sm fill-current">[300,300]</text>
-<text x="500" y="300" class="text-gray-500 text-sm fill-current">[500,300]</text>
-<text x="500" y="100" class="text-gray-500 text-sm fill-current">[500,100]</text>
-<text x="700" y="300" class="text-gray-500 text-sm fill-current">[700,300]</text>
-<text x="900" y="300" class="text-gray-500 text-sm fill-current">[900,300]</text>
-<text x="700" y="1300" class="text-gray-500 text-sm fill-current">[700,1300]</text>
-<text x="300" y="1300" class="text-gray-500 text-sm fill-current">[300,1300]</text>
-<text x="700" y="100" class="text-gray-500 text-sm fill-current">[700,100]</text>
-<text x="300" y="100" class="text-gray-500 text-sm fill-current">[300,100]</text>
-<text x="700" y="100" class="text-gray-500 text-sm fill-current">[700,100]</text>
-<text x="700" y="1300" class="text-gray-500 text-sm fill-current">[700,1300]</text>
-<text x="600" y="200" class="text-gray-500 text-sm fill-current">[600,200]</text>
-<text x="600" y="1200" class="text-gray-500 text-sm fill-current">[600,1200]</text>
-<text x="900" y="1100" class="text-gray-500 text-sm fill-current">[900,1100]</text>
-<text x="100" y="1100" class="text-gray-500 text-sm fill-current">[100,1100]</text>
+<script>
+	const boardLines = [
+		{ x1: '100', y1: '300', x2: '900', y2: '300' },
+		{ x1: '100', y1: '500', x2: '900', y2: '500' },
+		{ x1: '100', y1: '700', x2: '900', y2: '700' },
+		{ x1: '100', y1: '900', x2: '900', y2: '900' },
+		{ x1: '100', y1: '1100', x2: '900', y2: '1100' },
+		{ x1: '100', y1: '1100', x2: '100', y2: '300' },
+		{ x1: '300', y1: '1100', x2: '300', y2: '300' },
+		{ x1: '500', y1: '1300', x2: '500', y2: '100' },
+		{ x1: '700', y1: '1100', x2: '700', y2: '300' },
+		{ x1: '900', y1: '1100', x2: '900', y2: '300' },
+		{ x1: '100', y1: '700', x2: '700', y2: '1300' },
+		{ x1: '900', y1: '700', x2: '300', y2: '1300' },
+		{ x1: '100', y1: '700', x2: '700', y2: '100' },
+		{ x1: '900', y1: '700', x2: '300', y2: '100' },
+		{ x1: '300', y1: '100', x2: '700', y2: '100' },
+		{ x1: '300', y1: '1300', x2: '700', y2: '1300' },
+		{ x1: '400', y1: '200', x2: '600', y2: '200' },
+		{ x1: '400', y1: '1200', x2: '600', y2: '1200' },
+		{ x1: '100', y1: '300', x2: '900', y2: '1100' },
+		{ x1: '900', y1: '300', x2: '100', y2: '1100' }
+	];
+	const boardCoordinate = [
+		{ x: '100', y: '300' },
+		{ x: '100', y: '500' },
+		{ x: '300', y: '500' },
+		{ x: '500', y: '500' },
+		{ x: '700', y: '500' },
+		{ x: '100', y: '700' },
+		{ x: '300', y: '700' },
+		{ x: '500', y: '700' },
+		{ x: '700', y: '700' },
+		{ x: '100', y: '900' },
+		{ x: '300', y: '900' },
+		{ x: '500', y: '900' },
+		{ x: '700', y: '900' },
+		{ x: '100', y: '1100' },
+		{ x: '100', y: '1100' },
+		{ x: '300', y: '1100' },
+		{ x: '500', y: '1100' },
+		{ x: '500', y: '1300' },
+		{ x: '700', y: '1100' },
+		{ x: '300', y: '100' },
+		{ x: '300', y: '1300' },
+		{ x: '400', y: '200' },
+		{ x: '400', y: '1200' },
+		{ x: '900', y: '500' },
+		{ x: '900', y: '700' },
+		{ x: '900', y: '900' },
+		{ x: '300', y: '300' },
+		{ x: '500', y: '300' },
+		{ x: '500', y: '100' },
+		{ x: '700', y: '300' },
+		{ x: '900', y: '300' },
+		{ x: '700', y: '1300' },
+		{ x: '300', y: '1300' },
+		{ x: '700', y: '100' },
+		{ x: '300', y: '100' },
+		{ x: '700', y: '100' },
+		{ x: '700', y: '1300' },
+		{ x: '600', y: '200' },
+		{ x: '600', y: '1200' },
+		{ x: '900', y: '1100' },
+		{ x: '100', y: '1100' }
+	];
+</script>
+
+{#each boardLines as line}
+	<line
+		x1={line.x1}
+		y1={line.y1}
+		x2={line.x2}
+		y2={line.y2}
+		stroke="#525252"
+		stroke-linecap="round"
+		stroke-dasharray="6 6"
+	/>
+{/each}
+{#each boardCoordinate as coordinate}
+	<text x={coordinate.x} y={coordinate.y} class="text-gray-500 text-sm fill-current">
+		[{coordinate.x},{coordinate.y}]
+	</text>
+{/each}
--- src/lib/components/Pawn.svelte
+++ src/lib/components/Pawn.svelte
@@ -8,6 +8,7 @@
 	{cx}
 	{cy}
 	r="16"
+	class="cursor-pointer"
 	fill={color === 'red' ? '#FF005C' : '#426AF5'}
 	stroke={color === 'red' ? '#FF7BAB' : '#AAC7FF'}
 	stroke-width="6"
