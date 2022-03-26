fcf51dabea0eac0689c85b819254cb1a447262d8
Misbah Ansori <misbah.ansori24@gmail.com>
2022-03-26 07:43:24 +0700

    Add animation

--- src/lib/components/Pawn.svelte
+++ src/lib/components/Pawn.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { activePawn } from '$lib/store/state';
+
 	import { Color, type Pawn } from '$lib/types/coordinate.type';
 
 	import { createEventDispatcher } from 'svelte';
@@ -22,9 +24,20 @@
 	on:click={onClick}
 	{cx}
 	{cy}
-	r="16"
-	class="cursor-pointer"
+	r={$activePawn.x === cx && $activePawn.y === cy ? 24 : 16}
+	class="pawn"
+	class:selected={$activePawn.x === cx && $activePawn.y === cy}
 	fill={color === Color.RED ? '#FF005C' : '#426AF5'}
 	stroke={color === Color.RED ? '#FF7BAB' : '#AAC7FF'}
 	stroke-width="6"
 />
+
+<style>
+	.pawn {
+		cursor: pointer;
+		transition: all 500ms cubic-bezier(0.13, 0.67, 0.34, 0.93);
+	}
+	.pawn.selected {
+		transition: all 150ms cubic-bezier(0.13, 0.67, 0.34, 0.93);
+	}
+</style>
