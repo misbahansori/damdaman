82bcec6c40bdd1f46898f8fbfa8583d225a1fe51
Misbah Ansori <misbah.ansori24@gmail.com>
2022-03-29 16:28:10 +0700

    Revert "Add more test"
    This reverts commit 00c4d6e424f605aca4c4126165d7a7fe51b1c18c.

--- test/checkStraightLine.test.ts
+++ test/checkStraightLine.test.ts
@@ -10,15 +10,6 @@ test('should return true if the line is horizontal', () => {
 		])
 	).toBe(true);
 });
-test('should return true if the line is horizontal', () => {
-	expect(
-		checkStraightLine([
-			[700, 1100],
-			[700, 900],
-			[700, 700],
-		])
-	).toBe(true);
-});
 test('should return false if the last is coordinate is not align', () => {
 	expect(
 		checkStraightLine([
@@ -35,16 +26,6 @@ test('should return true if the line is diagonal', () => {
 			[300, 500],
 			[500, 300],
 			[675, 125],
-		])
-	).toBe(true);
-});
-
-test('should return true if the line is diagonal', () => {
-	expect(
-		checkStraightLine([
-			[900, 500],
-			[700, 500],
-			[500, 500],
 		])
 	).toBe(true);
 });
