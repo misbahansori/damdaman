4b4c7d699485eb8269483a54263bd310a160ce8e
Misbah Ansori <misbah.ansori24@gmail.com>
2022-04-03 10:58:18 +0700

    fix

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -176,7 +176,7 @@
 </svelte:head>
 
 <div class="w-full items-center min-h-screen bg-gray-100 relative">
-	<div class="wood bg-[url(img/wood-2.jpeg)] absolute inset-0" />
+	<div class="wood bg-[url(/img/wood-2.jpeg)] absolute inset-0" />
 	{#if $turn === Color.RED}
 		<div transition:fade={{ duration: 150 }} class="bg-red-500 inset-x-0 top-0 absolute h-4" />
 	{:else}
