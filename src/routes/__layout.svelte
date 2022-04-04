b354cb9cb2ae26f21a59875ad019661f5b650986
Misbah Ansori <misbah.ansori24@gmail.com>
2022-04-04 08:34:06 +0700

    Add github link

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,34 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<!-- Primary Meta Tags -->
+	<title>Dam Daman - Permainan Tradisional</title>
+	<meta name="title" content="Dam Daman - Permainan Tradisional" />
+	<meta
+		name="description"
+		content="Main permainan Dam daman disini. Dam daman adalah permainan mirip catur, setiap pemain harus bergantian menjalankan pionnya. Hanya ada dua pilhan, makan atau dimakan."
+	/>
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://damdaman.vercel.app/" />
+	<meta property="og:title" content="Dam Daman - Permainan Tradisional" />
+	<meta
+		property="og:description"
+		content="Main permainan Dam daman disini. Dam daman adalah permainan mirip catur, setiap pemain harus bergantian menjalankan pionnya. Hanya ada dua pilhan, makan atau dimakan."
+	/>
+	<meta property="og:image" content="https://damdaman.vercel.app/img/meta-image.jpg" />
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://damdaman.vercel.app/" />
+	<meta property="twitter:title" content="Dam Daman - Permainan Tradisional" />
+	<meta
+		property="twitter:description"
+		content="Main permainan Dam daman disini. Dam daman adalah permainan mirip catur, setiap pemain harus bergantian menjalankan pionnya. Hanya ada dua pilhan, makan atau dimakan."
+	/>
+	<meta property="twitter:image" content="https://damdaman.vercel.app/img/meta-image.jpg" />
+</svelte:head>
+
 <slot />
